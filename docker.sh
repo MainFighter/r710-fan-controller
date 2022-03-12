67f46b8b53db3370cd01971671997f142b0d8b5f
Main Fighter <MainFighter@users.noreply.github.com>
2022-03-12 11:21:29 +1100

    unraid compat change how started because lazy

--- docker.sh
+++ docker.sh
@@ -4,6 +4,9 @@ set -e
 
 TARGETDIR="/fancontroller"
 
+echo "*** Cloning fan controller repo..."
+git clone --branch docker https://github.com/MainFighter/r710-fan-controller.git "$TARGETDIR"
+
 echo "*** Creating and activating Python3 virtualenv..."
 if [ -d "$TARGETDIR/venv" ]; then
     echo "*** Existing venv found, purging it."
@@ -17,5 +20,8 @@ pip3 install -r requirements.txt
 
 echo "*** Deactivating Python3 virtualenv..."
 deactivate
+
+echo "*** Starting fan controller..."
+$TARGETDIR/venv/bin/python3 -u $TARGETDIR/fan_control.py --config $TARGETDIR/fan_control.yaml
 
 set +e
