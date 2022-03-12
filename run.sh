a2e43601622e3a3dc62867a31e16ce164951a987
Main Fighter <MainFighter@users.noreply.github.com>
2022-03-12 11:50:20 +1100

    stupid

--- run.sh
+++ run.sh
@@ -4,11 +4,12 @@ set -e
 
 TARGETDIR="/fancontroller"
 
-echo "*** Copy config file..."
-if [ ! -f "$TARGETDIR/fan_control.yaml" ]; then cp "/tmp/fan_control.yaml" "$TARGETDIR/fan_control.yaml" fi
+echo "*** Config file..."
+if [ ! -d "$TARGETDIR/fancontrol/config" ]; then mkdir $TARGETDIR/fancontrol/config fi
+if [ ! -f "$TARGETDIR/fan_control.yaml" ]; then cp "/tmp/fan_control.yaml" "$TARGETDIR/config/fan_control.yaml" fi
 if [ -f "/tmp/fan_control.yaml" ]; then rm "/tmp/fan_control.yaml" fi
 
 echo "*** Start fan controller..."
-"/fancontroller/venv/bin/python3" -u "/fancontroller/fan_control.py" --config "/fancontroller/fan_control.yaml"
+"/fancontroller/venv/bin/python3" -u "/fancontroller/fan_control.py" --config "/fancontroller/config/fan_control.yaml"
 
 set +e
