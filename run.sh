7bb9a9fc935b3d075e4331427901da95cdd59dff
Main Fighter <MainFighter@users.noreply.github.com>
2022-03-12 12:16:00 +1100

    fix

--- run.sh
+++ run.sh
@@ -5,9 +5,15 @@ set -e
 TARGETDIR="/fancontroller"
 
 echo "*** Config file..."
-if [ ! -d "$TARGETDIR/fancontrol/config" ]; then mkdir $TARGETDIR/fancontrol/config fi
-if [ ! -f "$TARGETDIR/fan_control.yaml" ]; then cp "/tmp/fan_control.yaml" "$TARGETDIR/config/fan_control.yaml" fi
-if [ -f "/tmp/fan_control.yaml" ]; then rm "/tmp/fan_control.yaml" fi
+if [ ! -d "$TARGETDIR/fancontrol/config" ]; then
+    mkdir $TARGETDIR/fancontrol/config
+fi
+if [ ! -f "$TARGETDIR/fan_control.yaml" ]; then
+    cp "/tmp/fan_control.yaml" "$TARGETDIR/config/fan_control.yaml"
+fi
+if [ -f "/tmp/fan_control.yaml" ]; then
+    rm "/tmp/fan_control.yaml"
+fi
 
 echo "*** Start fan controller..."
 "/fancontroller/venv/bin/python3" -u "/fancontroller/fan_control.py" --config "/fancontroller/config/fan_control.yaml"
